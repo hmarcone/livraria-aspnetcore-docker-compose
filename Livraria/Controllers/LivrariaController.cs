eae5bfbc1f280242651249f27538422c641c64a4
Humberto Marcone <humberto.freitas@squadra.com.br>
2021-08-30 17:44:20 -0300

    Adicionando a edição do livro.

--- Livraria/Controllers/LivrariaController.cs
+++ Livraria/Controllers/LivrariaController.cs
@@ -1,5 +1,6 @@
 ﻿using Livraria.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -45,6 +46,60 @@ namespace Livraria.Controllers
             }
             ModelState.AddModelError(string.Empty, $"Algo deu errado, modelo inválido");
             return View(livro);
+        }
+
+        // GET: Movies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var livro = await _context.Livros.FindAsync(id);
+
+            if (livro == null)
+            {
+                return NotFound();
+            }
+            return View(livro);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Titulo")] Livro livro)
+        {
+            if (id != livro.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(livro);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LivroExists(livro.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(livro);
+        }
+
+        private bool LivroExists(int id)
+        {
+            return _context.Livros.Any(e => e.Id == id);
         }
     }
 }
